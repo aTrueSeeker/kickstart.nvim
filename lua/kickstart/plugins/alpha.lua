ceb6192bc14d83dd28bdf37d04831a179b77182e
Akhil <akhilsathish96@gmail.com>
2025-12-19 03:51:46 +0530

    Adding alpha dashborad with disabling neotree opening on startup

---
+++ lua/kickstart/plugins/alpha.lua
@@ -0,0 +1,38 @@
+return {
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.startify'
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
+
+    alpha.setup(dashboard.opts)
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function()
+        if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv()[1]) == 1 then
+          vim.cmd 'Alpha'
+        end
+      end,
+    })
+  end,
+}
